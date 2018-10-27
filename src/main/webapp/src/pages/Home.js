837fb43562107420e150333f363afb265dd83f0e
Peter Yang <me@peteryang.io>
2018-10-27 03:55:03 -0700

    course list item

--- src/main/webapp/src/pages/Home.js
+++ src/main/webapp/src/pages/Home.js
@@ -5,6 +5,11 @@ import Paper from "@material-ui/core/Paper/Paper";
 import Typography from "@material-ui/core/Typography/Typography";
 import StudyGroups from "../components/home/StudyGroups";
 import CreateFab from "../components/home/CreateFab";
+import List from "@material-ui/core/List/List";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import ListItemText from "@material-ui/core/ListItemText/ListItemText";
 
 @withStyles({
     container: {
@@ -14,7 +19,7 @@ import CreateFab from "../components/home/CreateFab";
     courses: {
         flexGrow: 1,
         borderRadius: 0,
-        padding: "1rem"
+        paddingTop: "1rem",
     },
     coursesTitle: {
         textAlign: "center"
@@ -45,6 +50,14 @@ export default class Home extends React.Component {
                             className={classes.coursesTitle}>
                             Courses
                         </Typography>
+                        <List>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <Avatar>C</Avatar>
+                                </ListItemIcon>
+                                <ListItemText inset primary="CSCI" secondary="Computer Science"/>
+                            </ListItem>
+                        </List>
                     </Paper>
                     <section className={classes.groups}>
                         <StudyGroups courseId={this.state.selectedCourse}/>
